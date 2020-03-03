734d417858d08f65e7b9fa0c5fd29b9bbc2bb710
rajder <pawell1990@onet.eu>
2020-03-03 15:56:15 +0100

    create new repository

---
+++ src/components/HomeThreeColumns.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeThreeColumns = () => {
+    return (
+        <div>
+           <h2>Three Columns</h2>
+        </div>
+    );
+};
+
+export default HomeThreeColumns;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Logowanie</h2>
+        </div>
+    );
+};
+
+export default Login;
