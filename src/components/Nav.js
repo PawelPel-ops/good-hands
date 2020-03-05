d9f6f615bcb632b27964d5c9180654cd9bddb498
rajder <pawell1990@onet.eu>
2020-03-05 13:23:53 +0100

    add navigation

---
+++ src/components/Nav.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Link, Route, Switch} from "react-router-dom";
+import Login from "./Login";
+import Registration from "./Registration";
+import {Link as LinkScroll} from "react-scroll";
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <nav>
+                <ul className="login_nav">
+                    <li><Link to="/login">
+                        Zaloguj
+                    </Link></li>
+                    <li><Link to="/registration">
+                        Załóż konto
+                    </Link></li>
+                    <Switch>
+                        <Route path="/login" component={Login} />
+                        <Route path="/registration" component={Registration} />
+                    </Switch>
+                </ul>
+                <ul className="scroll_nav">
+
+                        <li><LinkScroll
+                            activeClass="active"
+                            to="start"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}>Start</LinkScroll></li>
+                        <li><LinkScroll
+                            activeClass="active"
+                            to="whatIsGoingOn"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}>O co chodzi?</LinkScroll></li>
+                        <li><LinkScroll
+                            activeClass="active"
+                            to="aboutUs"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}>O nas</LinkScroll></li>
+                        <li><LinkScroll
+                            activeClass="active"
+                            to="foundation"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}>Fundacja i organizacje</LinkScroll></li>
+                        <li><LinkScroll
+                            activeClass="active"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}>Kontakt</LinkScroll></li>
+                    </ul>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
